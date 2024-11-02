85c5543b6b823550429835f509d5cefae5826927
squareerootsofjon <olddfacedeb@jon.com>
2024-11-02 10:56:32 +0700

    new in gnu gnu linux  no spy spy

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -20,13 +20,6 @@ $.each(sectionArray, function(index, value){
          
      });
     
-    $('.click-scroll').eq(index).click(function(e){
-        var offsetClick = $('#' + 'section_' + value).offset().top - 75;
-        e.preventDefault();
-        $('html, body').animate({
-            'scrollTop':offsetClick
-        }, 300)
-    });
     
 });
 
